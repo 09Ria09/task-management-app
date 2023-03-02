4a0b0a1adcc88eed49611226e6c8c63080270221
Edsard Grisel <E.G.Grisel@student.tudelft.nl>
2023-03-02 12:31:07 +0100

    Adding TaskList class

---
+++ commons/src/main/java/commons/TaskList.java
@@ -0,0 +1,59 @@
+package commons;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.*;
+
+
+public class TaskList {
+    String name;
+    ArrayList<Task> tasks;
+
+    public TaskList(String name) {
+        this.name = name;
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    @Override
+    public String toString() {
+        String string = "";
+        for(int i = 0; i < tasks.size(); i++) {
+            string += tasks.get(i) + "/n";
+        }
+        return string;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+
+}
