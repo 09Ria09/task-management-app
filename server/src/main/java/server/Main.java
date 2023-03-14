36d92f3395589f7b52355fd1e2055485236bee8a
Teun Bosch <T.G.H.Bosch@student.tudelft.nl>
2023-03-14 16:16:45 +0100

    I forgot to remove some of the imports I did while practising a bit with spring

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -18,8 +18,6 @@ package server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "commons", "server" })
