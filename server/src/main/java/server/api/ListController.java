05091cfe0be8f1200ed9ff553e2e6dd56fd878f0
Edsard Grisel <E.G.Grisel@student.tudelft.nl>
2023-03-17 15:10:46 +0100

    copy over all the javadoc from board to list

--- server/src/main/java/server/api/ListController.java
+++ server/src/main/java/server/api/ListController.java
@@ -27,11 +27,23 @@ public class ListController {
         listService.addList(boardid, taskList);
     }
 
+    /**
+     * Get util method to get all the tasklists from a specific board
+     * @param boardid the id of the board that will be accesses
+     * @return a list of tasklists from the corresponding board
+     */
     @GetMapping("/{boardid}/tasklists")
     public List<TaskList> getTaskLists(@PathVariable("boardid") final long boardid) {
         return listService.getLists(boardid);
     }
 
+    /**
+     * Get util method for a specific list of a specific board
+     *
+     * @param boardid the id of the board from which we want to access the list
+     * @param taskListid the id of the list that will be accessed
+     * @return the corresponding list from the corresponding board
+     */
     @GetMapping("/{boardid}/tasklist/{taskListid}")
     public TaskList getTaskList(
             @PathVariable("boardid") final long boardid,
@@ -40,6 +52,13 @@ public class ListController {
         return listService.getList(boardid, taskListid);
     }
 
+    /**
+     * Put request to change the name of a tasklist
+     *
+     * @param boardid the id of the board from where the list will be renamed
+     * @param tasklistid the id of the list of which the name will be changed
+     * @param name the new name for the list
+     */
     @PutMapping("/{boardid}/{tasklistid}")
     public void renameTaskList(
             @PathVariable("boardid") final long boardid,
