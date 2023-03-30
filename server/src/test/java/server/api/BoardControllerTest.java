08af41011e163479b62f62fc2d4d8cc732c4103b
Dorian Herbiet <d.n.g.herbiet@student.tudelft.nl>
2023-03-30 11:35:21 +0200

    Add more endpoint Tests

--- server/src/test/java/server/api/BoardControllerTest.java
+++ server/src/test/java/server/api/BoardControllerTest.java
@@ -1,9 +1,9 @@
 package server.api;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import commons.Board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
@@ -92,25 +93,34 @@ public class BoardControllerTest {
 
     @Test
     public void testDeleteBoardEndpoint() throws Exception {
-        Board board = new Board("Test Board", new ArrayList<>(), new ArrayList<>());
-
-        List<Board> boards = Arrays.asList(board, new Board("Board 1", new ArrayList<>(), new ArrayList<>()));
         Mockito.doNothing().when(boardService).removeBoardByID(1);
-
 
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/boards/1"))
                 .andExpect(status().isOk());
-
         Mockito.verify(boardService, Mockito.times(1)).removeBoardByID(1);
     }
 
     @Test
     public void testRenameBoardEndpoint() throws Exception {
-        List<Board> boards = List.of(new Board("Board 1", new ArrayList<>(), new ArrayList<>()));
         Mockito.doNothing().when(boardService).renameBoard(1, "Test 2");
+
         mockMvc.perform(MockMvcRequestBuilders.put("/api/boards/1")
                         .param("name", "Test 2"))
                 .andExpect(status().isOk());
         Mockito.verify(boardService, Mockito.times(1)).renameBoard(1, "Test 2");
+    }
+
+    @Test
+    public void testAddBoardEndpoint() throws Exception {
+        Board newBoard = new Board("New Board", List.of(), List.of());
+        String requestBody = new ObjectMapper().writeValueAsString(newBoard);
+        Mockito.when(boardService.addBoard(newBoard)).thenReturn(newBoard);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/boards/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name", is("New Board")));
+        Mockito.verify(boardService, Mockito.times(1)).addBoard(newBoard);
     }
 }
