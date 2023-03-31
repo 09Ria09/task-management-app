2f88f65a88dafe8b43886008486fcb6f4fde48f1
Rares Iordan <R.Iordan@student.tudelft.nl>
2023-03-31 16:08:26 +0200

    added tests server side

--- server/src/test/java/server/api/ListControllerTest.java
+++ server/src/test/java/server/api/ListControllerTest.java
@@ -15,9 +15,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import server.services.BoardService;
 import server.services.ListService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -89,5 +88,126 @@ public class ListControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("name", is("New List")));
         Mockito.verify(listService, Mockito.times(1)).addList(1, newList);
+    }
+
+    @Test
+    public void testAddTaskListEndpointBadRequest() throws Exception {
+        TaskList newList = new TaskList("");
+        String requestBody = new ObjectMapper().writeValueAsString(newList);
+        Mockito.when(listService.addList(1, newList)).thenReturn(newList);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/lists/1/tasklist")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isBadRequest());
+        Mockito.verify(listService, Mockito.times(0))
+            .addList(1, newList);
+    }
+
+    @Test
+    public void testAddTaskListEndpointNotFound() throws Exception {
+        TaskList newList = new TaskList("New List");
+        String requestBody = new ObjectMapper().writeValueAsString(newList);
+        Mockito.when(listService.addList(1, newList)).thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/lists/1/tasklist")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isNotFound());
+        Mockito.verify(listService, Mockito.times(1))
+            .addList(1, newList);
+    }
+
+    @Test
+    public void getTaskListsEndpointNotFound() throws Exception {
+        Mockito.when(listService.getLists(1)).thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/lists/1/tasklists"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(listService, Mockito.times(1)).getLists(1);
+    }
+
+    @Test
+    public void getTaskListEndpointNotFound() throws Exception {
+        Mockito.when(listService.getList(1, 2)).thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/lists/1/tasklist/2"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(listService, Mockito.times(1)).getList(1, 2);
+    }
+
+    @Test
+    public void renameTaskListEndpointBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/lists/1/2")
+                .param("name", ""))
+            .andExpect(status().isBadRequest());
+        Mockito.verify(listService, Mockito.times(0))
+            .renameList(1, 2, "");
+    }
+
+    @Test
+    public void renameTaskListEndpointNotFound() throws Exception {
+        Mockito.when(listService.renameList(1, 2, "Name"))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/lists/1/2")
+                .param("name", "Name"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(listService, Mockito.times(1))
+            .renameList(1, 2, "Name");
+    }
+
+    @Test
+    public void deleteTaskListEndpointNotFound() throws Exception {
+        Mockito.when(listService.removeListByID(1, 2))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/lists/1/2"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(listService, Mockito.times(1))
+            .removeListByID(1, 2);
+    }
+    @Test
+    public void deleteTaskListEndpointInternalServerError() throws Exception {
+        Mockito.when(listService.removeListByID(1, 2))
+            .thenThrow(RuntimeException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/lists/1/2"))
+            .andExpect(status().isInternalServerError());
+
+        Mockito.verify(listService, Mockito.times(1))
+            .removeListByID(1, 2);
+    }
+
+    @Test
+    public void reorderTasksEndpointOk() throws Exception {
+        TaskList taskList = new TaskList("Test TaskList");
+        Mockito.when(listService.reorderTask(1L, 2L, 3L, 1))
+            .thenReturn(taskList);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/lists/1/2/reorder/3")
+                .param("newIndex", "1"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("name", is("Test TaskList")));
+
+        Mockito.verify(listService, Mockito.times(1))
+            .reorderTask(1L, 2L, 3L, 1);
+    }
+
+    @Test
+    public void reorderTaskEndpointNotFound() throws Exception {
+        Mockito.when(listService.reorderTask(1L, 2L, 3L, 1))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/lists/1/2/reorder/3")
+                .param("newIndex", "1"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(listService, Mockito.times(1))
+            .reorderTask(1L, 2L, 3L, 1);
     }
 }
--- server/src/test/java/server/api/TaskControllerTest.java
+++ server/src/test/java/server/api/TaskControllerTest.java
@@ -17,6 +17,7 @@ import server.services.ListService;
 import server.services.TaskService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -94,4 +95,152 @@ public class TaskControllerTest {
                 .andExpect(jsonPath("name", is("New Task")));
         Mockito.verify(taskService, Mockito.times(1)).addTask(1, 2, newTask);
     }
+    @Test
+    public void testAddTaskEndpointBadRequest() throws Exception {
+        Task newTask = new Task("", "");
+        String requestBody = new ObjectMapper().writeValueAsString(newTask);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/tasks/1/2/task")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isBadRequest());
+        Mockito.verify(taskService, Mockito.times(0))
+            .addTask(1, 2, newTask);
+    }
+
+    @Test
+    public void testAddTaskEndpointNotFound() throws Exception {
+        Task newTask = new Task("New Task", "");
+        String requestBody = new ObjectMapper().writeValueAsString(newTask);
+        Mockito.when(taskService.addTask(1, 2, newTask))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/tasks/1/2/task")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .addTask(1, 2, newTask);
+    }
+
+    @Test
+    public void testGetAllTasksEndpointNotFound() throws Exception {
+        Mockito.when(taskService.getTasks(1, 2))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/tasks/1/tasklist/2/tasks"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .getTasks(1, 2);
+    }
+
+    @Test
+    public void testGetTaskEndpointNotFound() throws Exception {
+        Mockito.when(taskService.getTask(1, 2, 3))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/tasks/1/tasklist/2/task/3"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .getTask(1, 2, 3);
+    }
+
+    @Test
+    public void testRenameTaskEndpointBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/tasks/1/2/3")
+                .param("name", "  "))
+            .andExpect(status().isBadRequest());
+
+        Mockito.verify(taskService, Mockito.times(0))
+            .renameTask(1, 2, 3, "  ");
+    }
+
+    @Test
+    public void testRenameTaskEndpointNotFound() throws Exception {
+        Mockito.when(taskService.renameTask(1, 2, 3, "New Name"))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/tasks/1/2/3")
+                .param("name", "New Name"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .renameTask(1, 2, 3, "New Name");
+    }
+
+    @Test
+    public void testRenameTaskEndpointInternalServerError() throws Exception {
+        Mockito.when(taskService.renameTask(1, 2, 3, "New Name"))
+            .thenThrow(RuntimeException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/tasks/1/2/3")
+                .param("name", "New Name"))
+            .andExpect(status().isInternalServerError());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .renameTask(1, 2, 3, "New Name");
+    }
+
+    @Test
+    public void testDeleteTaskEndpointNotFound() throws Exception {
+        Mockito.when(taskService.removeTaskById(1, 2, 3))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/tasks/1/2/3"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .removeTaskById(1, 2, 3);
+    }
+
+    @Test
+    public void testDeleteTaskEndpointInternalServerError() throws Exception {
+        Mockito.when(taskService.removeTaskById(1, 2, 3))
+            .thenThrow(Exception.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/tasks/1/2/3"))
+            .andExpect(status().isInternalServerError());
+        Mockito.verify(taskService, Mockito.times(1))
+            .removeTaskById(1, 2, 3);
+    }
+
+    @Test
+    public void testEditDescriptionEndpointBadRequest() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/tasks/1/2/desc/3")
+                .param("description", (String) null))
+            .andExpect(status().isBadRequest());
+        Mockito.verify(taskService, Mockito.times(0))
+            .editDescription(1, 2, 3, null);
+    }
+
+    @Test
+    public void testEditDescriptionEndpointNotFound() throws Exception {
+        Mockito.when(taskService.editDescription(1, 2, 3, "New description"))
+            .thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/tasks/1/2/desc/3")
+                .param("description", "New description"))
+            .andExpect(status().isNotFound());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .editDescription(1, 2, 3, "New description");
+    }
+
+    @Test
+    public void testEditDescriptionEndpointInternalServerError() throws Exception {
+        Mockito.when(taskService.editDescription(1, 2, 3, "New description"))
+            .thenThrow(RuntimeException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/tasks/1/2/desc/3")
+                .param("description", "New description"))
+            .andExpect(status().isInternalServerError());
+
+        Mockito.verify(taskService, Mockito.times(1))
+            .editDescription(1, 2, 3, "New description");
+    }
+
 }
