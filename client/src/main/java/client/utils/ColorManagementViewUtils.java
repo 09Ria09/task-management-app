e2b4cad8472f8df10cd5dc6f19b816b8c35622d0
Edsard Grisel <E.G.Grisel@student.tudelft.nl>
2023-04-03 10:44:03 +0200

    Board colors completed and checktyle fix

---
+++ client/src/main/java/client/utils/ColorManagementViewUtils.java
@@ -0,0 +1,16 @@
+package client.utils;
+
+import commons.TaskPreset;
+
+public class TaskPresetUtils {
+
+    private final ServerUtils server;
+
+    public TaskPresetUtils(ServerUtils server) {
+        this.server = server;
+    }
+
+    public TaskPreset addTaskPreset() {
+        return null;
+    }
+}
